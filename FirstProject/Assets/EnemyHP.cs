e524c40dde49c264c84bb32f2f753f120fce9e41
Jgithublol <115186168+Jgithublol@users.noreply.github.com>
2022-10-10 12:12:34 -0700

    Backup 1

---
+++ FirstProject/Assets/EnemyHP.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHP : MonoBehaviour
+{
+    public int hp = 4;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "PlayerBullet")
+        {
+            hp--;
+            if (hp <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ My project/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public GameObject prefab;
+    public float bulletSpeed = 10.0f;
+    public float bulletLifetime = 2.0f;
+    public float shootDelay = 1.0f;
+    float timer = 0;
+
+    public AudioClip shootSound;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if(timer >= shootDelay)
+            {
+                if(Time.timeScale > 0)
+                {
+                    GameObject bullet = Instantiate(prefab, transform.position, Quaternion.identity);
+                    Vector3 mousePosition = Input.mousePosition;
+                    mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+                    Vector3 shootDir = mousePosition - transform.position;
+                    shootDir.Normalize();
+                    bullet.GetComponent<Rigidbody2D>().velocity = shootDir * bulletSpeed;
+                    Destroy(bullet, bulletLifetime);
+
+                    Camera.main.GetComponent<AudioSource>().PlayOneShot(shootSound);
+                }
+            }
+        }
+    }
+}
